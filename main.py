ad361be88fef65560cb1f2acf3c2be143b666ea2
Andy Roxby <aroxby@users.noreply.github.com>
2024-12-05 17:00:52 -0500

    Break out serializer package

--- main.py
+++ main.py
@@ -1,212 +1,37 @@
 #!/usr/bin/env python3
-from abc import ABC, abstractmethod
 from dataclasses import asdict, dataclass
 from pathlib import Path
 import sys
 from xml.etree import ElementTree
 
-STRING_CODEC = 'cp437'
-
-
-class ValidationError(Exception):
-    pass
-
-
-class Field(ABC):
-    def __init__(self, validator=None):
-        self.validator = validator
-
-    def validate(self, value):
-        return self.validator(value) if self.validator else None
-
-    @abstractmethod
-    def deserialize(self, attributes: dict, data: bytes):
-        raise NotImplementedError
-
-    @abstractmethod
-    def serialize(self, obj) -> bytes:
-        raise NotImplementedError
-
-
-class ByteArrayField(Field):
-    def __init__(self, length: int, validator=None):
-        super().__init__(validator)
-        self.length = length
-
-    def deserialize(self, attributes: dict, data: bytes) -> (bytes, int):
-        return data[:self.length], self.length
-
-    def serialize(self, obj) -> bytes:
-        return obj[:self.length]
-
-
-class UInt16Field(Field):
-    length = 2
-
-    def deserialize(self, attributes: dict, data: bytes) -> (int, int):
-        return int.from_bytes(data[:self.length], byteorder='little', signed=False), self.length
-
-    def serialize(self, obj) -> bytes:
-        return obj.to_bytes(length=self.length, byteorder='little', signed=False)
-
-
-class UInt32Field(UInt16Field):
-    length = 4
-
-
-class ZStringField(Field):
-    def deserialize(self, attributes: dict, data: bytes) -> (str, int):
-        mbs, _ = data.split(b'\0', 1)
-        return mbs.decode(STRING_CODEC), len(mbs) + 1
-
-    def serialize(self, obj) -> bytes:
-        return obj.encode(STRING_CODEC) + b'\0'
-
-
-class DynamicStringField(Field):
-    length = 0  # Length isn't known until other fields are deserialized
-
-    def deserialize(self, attributes: dict, data: bytes) -> (str, int):
-        mbs = data[:self.length]
-        return mbs.decode(STRING_CODEC), len(mbs)
-
-    def serialize(self, obj) -> bytes:
-        return obj.encode(STRING_CODEC)
-
-
-class FixedStringField(DynamicStringField):
-    def __init__(self, length: int, validator=None):
-        super().__init__(validator)
-        self.length = length
-
-
-class ReverseFixedStringField(FixedStringField):
-    def deserialize(self, attributes: dict, data: bytes) -> (str, int):
-        value, length = super().deserialize(attributes, data)
-        value = value[::-1]
-        return value, length
-
-    def serialize(self, obj) -> bytes:
-        return super().serialize(obj[::-1])
-
-
-class DynamicListField(Field):
-    length = 0  # Length isn't known until other fields are deserialized
-
-    def __init__(self, element_field: Field, validator=None):
-        super().__init__(validator)
-        self.element_field = element_field
-
-    def deserialize(self, attributes: dict, data: bytes) -> (list, int):
-        offset = 0
-        elements = []
-
-        for _ in range(self.length):
-            element, element_length = self.element_field.deserialize(attributes, data[offset:])
-            elements.append(element)
-            offset += element_length
-
-        return elements, offset
-
-    def serialize(self, obj) -> bytes:
-        data = b''
-        for element in obj:
-            data += self.element_field.serialize(element)
-        return data
-
-
-class EncodedLengthField(Field):
-    def __init__(self, length_field: Field, element_field: Field, validator=None):
-        super().__init__(validator)
-        self.length_field = length_field
-        self.element_field = element_field
-
-    def deserialize(self, attributes: dict, data: bytes):
-        length, offset = self.length_field.deserialize(attributes, data)
-        self.element_field.length = length
-        element, element_length = self.element_field.deserialize(attributes, data[offset:])
-        return element, offset + element_length
-
-    def serialize(self, obj) -> bytes:
-        data = self.length_field.serialize(len(obj))
-        data += self.element_field.serialize(obj)
-        return data
-
-
-class SerializerMeta(type):
-    @staticmethod
-    def _get_fields(attrs: dict) -> dict:
-        # TODO: Sort fields? Then use an OrderedDict for deterministic ordering
-        fields = {key: value for key, value in attrs.items() if isinstance(value, Field)}
-        return fields
-
-    def __new__(cls, name, bases, attrs, **kwds):
-        attrs['fields'] = cls._get_fields(attrs)
-        return super().__new__(cls, name, bases, attrs)
-
-
-class Serializer(metaclass=SerializerMeta):
-    fields = {}
-
-    # TODO: Drop `attributes`
-    def deserialize(self, attributes: dict, data: bytes) -> (dict, int):
-        attrs = {}
-        offset = 0
-        for name, field in self.fields.items():
-            attrs[name], field_length = field.deserialize(attributes, data[offset:])
-            offset += field_length
-        return attrs, offset
-
-    def serialize(self, attrs: dict) -> bytes:
-        data = b''
-        for name, field in self.fields.items():
-            data += field.serialize(attrs[name])
-        return data
-
-
-class SerializerField(Field):
-    def __init__(self, serializer: Serializer, validator=None):
-        super().__init__(validator)
-        self.serializer = serializer
-
-    def deserialize(self, attributes: dict, data: bytes) -> (dict, int):
-        return self.serializer.deserialize(attributes, data)
-
-    def serialize(self, obj) -> bytes:
-        return self.serializer.serialize(obj)
-
-
-def file_magic_validator(magic: bytes):
-    def validator(value: bytes):
-        if value != magic:
-            raise ValidationError
-    return validator
+from structures import fields
+from structures.serializers import Serializer
 
 
 class DocumentHeaderAttributeSerializer(Serializer):
-    key = EncodedLengthField(UInt16Field(), DynamicStringField())
-    locale = ReverseFixedStringField(4)
-    value = EncodedLengthField(UInt16Field(), DynamicStringField())
+    key = fields.EncodedLengthField(fields.UInt16Field(), fields.DynamicStringField())
+    locale = fields.ReverseFixedStringField(4)
+    value = fields.EncodedLengthField(fields.UInt16Field(), fields.DynamicStringField())
 
 
 class DocumentHeaderSerializer(Serializer):
     # H2CS (StarCraft 2 Header)
-    map_magic = ByteArrayField(4, file_magic_validator(b'H2CS'))
+    map_magic = fields.ByteArrayField(4, fields.file_magic_validator(b'H2CS'))
     # \x8\0\0\0 (record break?)
-    unk1 = ByteArrayField(4)
+    unk1 = fields.ByteArrayField(4)
     # 2S\0\0 (StarCraft 2)
-    game_magic = ByteArrayField(4, file_magic_validator(b'2S\0\0'))
+    game_magic = fields.ByteArrayField(4, fields.file_magic_validator(b'2S\0\0'))
     # \x8\0\0\0 (record break?)
-    unk2 = ByteArrayField(4)
+    unk2 = fields.ByteArrayField(4)
     # \xe1\x38\x1\0\xe1\x38\x1\0
-    unk3 = ByteArrayField(8)
+    unk3 = fields.ByteArrayField(8)
     # ???
-    unk4 = ByteArrayField(20)
+    unk4 = fields.ByteArrayField(20)
     # Map dependencies (eg: bnet:Swarm Story (Campaign)/0.0/999,file:Campaigns/SwarmStory.SC2Campaign)
-    dependencies = EncodedLengthField(UInt32Field(), DynamicListField(ZStringField()))
+    dependencies = fields.EncodedLengthField(fields.UInt32Field(),fields. DynamicListField(fields.ZStringField()))
     # Map attributes (DocumentHeaderAttribute)
-    attribs = EncodedLengthField(
-        UInt32Field(), DynamicListField(SerializerField(DocumentHeaderAttributeSerializer()))
+    attribs = fields.EncodedLengthField(
+        fields.UInt32Field(), fields.DynamicListField(fields.SerializerField(DocumentHeaderAttributeSerializer()))
     )
 
 
